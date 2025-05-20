281f97bf77077122306320ed12420629db15926f
mrsharma7777 <127834732+mrsharma7777@users.noreply.github.com>
2025-05-20 13:24:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/auth", authRoutes);
-const express = require('express');
+
 const cors = require('cors');
 
 const pool = new Pool({
