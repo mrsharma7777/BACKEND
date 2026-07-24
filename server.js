69b843135c1ff0f3676452cbb7acd93f562708cf
Paradise Tech <127834732+mrsharma7777@users.noreply.github.com>
2026-07-24 13:33:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const pool = new Pool({
 app.use(cors());
 app.use(express.json());
 
-// Store all live clients
+// Store all live client
 let liveClients = [];
 
 io.on("connection", (socket) => {
