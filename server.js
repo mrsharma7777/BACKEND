800b6e7905da907d82e1aa6220e6e3842c289512
Paradise Tech <127834732+mrsharma7777@users.noreply.github.com>
2026-09-09 17:37:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/auth", authRoutes);
 
 const pool = new Pool({
 host: "dpg-dagkinp5efls73b03qug-a.singapore-postgres.render.com",
-  user: "database_sk8u_user,
+  user: "database_sk8u_user",
   port: 5432,
   password: "1EouH2V7dz4Jrbp4fSkI7pPYJKc2HQT3",// Set your actual DB password
   database: "database_sk8u",
